55a004ed141c85f6d8212c5bb05ebd57c7faea41
agustina <asturla@itba.edu.ar>
2026-06-15 20:03:51 -0300

    fix: corregir path CartContext

--- gloslip/src/app/pago-completado/page.js
+++ gloslip/src/app/pago-completado/page.js
@@ -2,7 +2,7 @@
 import { Suspense, useEffect } from "react";
 import { useSearchParams } from "next/navigation";
 import Link from "next/link";
-import { useCart } from "../../../context/CartContext";
+import { useCart } from "../context/CartContext";
 
 function PagoCompletadoContent() {
   const params = useSearchParams();
